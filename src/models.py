1e3fa538ec114c9794b6d66c80610895320ea09f
Aquino28 <aquino.edu2028@gmail.com>
2026-08-14 22:04:31 +0800

    Fix efficientnet imagenet weights.

--- src/models.py
+++ src/models.py
@@ -22,12 +22,12 @@ def build_efficientnet(
     num_classes=3,
     dropout_rate=0.4,
     use_metadata=False,
-    weights="imagenet",
+    weights="imagenet"
 ):
     image_input = layers.Input(shape=input_image_shape, name="image_input")
     base_model = EfficientNetB3(
         include_top=False,
-        weights="weights",
+        weights="imagenet",
         input_tensor=image_input,
         name="efficientnetb3_backbone",
     )
